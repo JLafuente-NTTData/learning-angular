065ff1be78120e0226f8e49b79fe1e2f83cf6371
Jose Lafuente <jose.lafuentecolorado.st@emeal.nttdata.com>
2026-02-17 09:51:21 +0100

    fix(tarjeta de producto): implemetación correcta del DetailCard

--- src/app/components/detail-product-card/detail-product-card.ts
+++ src/app/components/detail-product-card/detail-product-card.ts
@@ -1,6 +1,5 @@
-import { Component, inject, input, signal, OnInit } from '@angular/core';
-import { ProductService, Product } from '../../services/product';
-import { ActivatedRoute } from '@angular/router';
+import { Component, input } from '@angular/core';
+import { Product } from '../../services/product';
 
 @Component({
   selector: 'app-detail-product-card',
@@ -8,31 +7,16 @@ import { ActivatedRoute } from '@angular/router';
   template: `
     <h3>Detalle del producto</h3>
 
-    @if (product) {
-      <div class="product-card">
-        <img [src]="product.image" [alt]="product.title" style="width: 100px;" />
-        <h4>{{ product.title }}</h4>
-        <p>{{ product.description }}</p>
-        <p><strong>Precio:</strong> {{ product.price }}</p>
-        <p>ID del producto: {{ product.id }}</p>
-      </div>
-    } @else {
-      <p>Cargando producto o no encontrado...</p>
-    }
+    <div class="product-card">
+      <img [src]="product().image" [alt]="product().title" style="width: 100px;" />
+      <h4>{{ product().title }}</h4>
+      <p>{{ product().description }}</p>
+      <p><strong>Precio:</strong> {{ product().price }}</p>
+      <p>ID del producto: {{ product().id }}</p>
+    </div>
   `,
   styleUrls: ['./detail-product-card.css'],
 })
-export class DetailProductCard implements OnInit {
-  product: Product | null = null;
-  private route = inject(ActivatedRoute);
-  private productService = inject(ProductService);
-
-  ngOnInit() {
-    const id = 1;
-    this.productService.getProductById(id).subscribe({
-      next: (data) => {
-        this.product = data;
-      },
-    });
-  }
+export class DetailProductCard {
+  product = input.required<Product>();
 }
--- src/app/pages/detail-product-page/detail-product-page.ts
+++ src/app/pages/detail-product-page/detail-product-page.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { DetailProductCard } from '../../components/detail-product-card/detail-product-card';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService, Product } from '../../services/product'; // Importamos el servicio para obtener los detalles del producto
 
 @Component({
   selector: 'app-detail-product-page',
+  standalone: true,
+  imports: [DetailProductCard],
   template: `
     <h2>Product Detail</h2>
     <p>This is the product detail page.</p>
+    @if (product(); as product) {
+      <app-detail-product-card [product]="product"></app-detail-product-card>
+    } @else {
+      <p>Cargando producto</p>
+    }
   `,
 })
-export class DetailProductPage {}
+export class DetailProductPage implements OnInit {
+  private activatedRoute = inject(ActivatedRoute);
+  private productService = inject(ProductService);
+
+  product = signal<Product | null>(null);
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe((params) => {
+      const id = params['id'];
+      this.productService.getProductById(id).subscribe({
+        next: (data) => {
+          this.product.set(data);
+        },
+      });
+    });
+  }
+}
